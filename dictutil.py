2ad229e1221e998e06357c76219a9e6e3ecc22cf
Lguyogiro <robert.pugh408@gmail.com>
2016-12-08 17:48:04 -0800

    overload operators with merge

--- dictutil.py
+++ dictutil.py
@@ -1,15 +1,35 @@
+from operator import add, sub, mul, div
+
+
+def merged(d1, d2, func=None):
+    """
+    update a copy of d1 with d2. For evey key they have in
+    common, combine the respective values using func.
+    """
+    d = d1.copy()
+    if func is None:
+        d.update(d2)
+    else:
+        for k in d2:
+            if k in d:
+                d[k] = func(d[k], d2[k])
+            else:
+                d[k] = d2[k]
+    return d
+
+
 class BetterDict(dict):
     def __add__(self, other):
-        pass
-    
+        return merged(self, other, add)
+
     def __sub__(self, other):
-        pass
+        return merged(self, other, sub)
 
     def __mul__(self, other):
-        pass
+        return merged(self, other, mul)
 
     def __div__(self, other):
-        pass
+        return merged(self, other, div)
 
     def __gt__(self, other):
         pass
@@ -18,6 +38,10 @@ class BetterDict(dict):
         pass
 
     def merge(self, other, func=None):
+        """
+        In-place update of self with other, but when they have a key in common,
+        combine their values useing func.
+        """
         if func is None:
             self.update(other)
         for k in other:
@@ -26,3 +50,5 @@ class BetterDict(dict):
             else:
                 self[k] = other[k]
 
+if __name__ == '__main__':
+    pass
